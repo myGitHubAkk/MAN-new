79e5dc80044fec520a333e9c8931d51189c14176
myGitHubAkk <denusakkgit@gmail.com>
2021-08-05 19:00:18 +0300

    create org game

--- lib/presentation/game_screen/original_game_sreen.dart
+++ lib/presentation/game_screen/original_game_sreen.dart
@@ -10,6 +10,30 @@ class OriginalGameScreen extends StatefulWidget {
 class _OriginalGameScreenState extends State<OriginalGameScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final widthScreen = MediaQuery.of(context).size.width;
+    final sizeCanvas = widthScreen * 0.8;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 20,
+            ),
+            Container(
+              width: sizeCanvas,
+              height: sizeCanvas,
+              child: GestureDetector(
+                  // child: CustomPaint(painter: ,),
+                  ),
+            ),
+            Container(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
