40a59463c3c8866cf80b167b634ba4a36179878e
myGitHubAkk <denusakkgit@gmail.com>
2021-09-12 23:14:47 +0300

    create song present

---
+++ lib/entities/subjects/song_present.dart
@@ -0,0 +1,34 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:man_project/data/terms.dart';
+import 'package:man_project/domain/create_position.dart';
+import 'package:man_project/entities/subjects/subject.dart';
+import 'package:man_project/entities/user_term.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+class SongPresent extends Subject {
+  CreatePosition _createPosition = CreatePosition();
+  AudioCache audioPlayer = AudioCache();
+
+  @override
+  int get position => _createPosition.posiiton;
+
+  @override
+  void snakeClash() {
+    //print(AudioPlayer.players);
+    audioPlayer.play('slipknot_-_unsainted.mp3');
+  }
+
+  @override
+  void gameOver() {
+    _createPosition = CreatePosition();
+  }
+
+  static void randomNextAnswer() {}
+
+  @override
+  Widget get widget {
+    return cellTemlate(colorLight: Colors.yellow, colorDark: Colors.green);
+  }
+}
--- lib/entities/subjects/subjects.dart
+++ lib/entities/subjects/subjects.dart
@@ -1,18 +1,17 @@
 import 'package:man_project/entities/subjects/apple.dart';
+import 'package:man_project/entities/subjects/song_present.dart';
 import 'package:man_project/entities/subjects/stone.dart';
 import 'package:man_project/entities/subjects/subject.dart';
 
 class Subjects {
   static List<Subject> subjects = [
     Stone(),
+    Stone(),
+    Stone(),
+    Stone(),
     AppleWithWords(),
     AppleWithWords(),
     AppleWithWords(),
-    AppleWithWords(),
-    AppleWithWords(),
-    AppleWithWords(),
-    AppleWithWords(),
-    AppleWithWords(),
-    AppleWithWords(),
+    SongPresent(),
   ];
 }
