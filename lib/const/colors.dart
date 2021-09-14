a4239663b0adcecb72bac4815ef43a4fac308cc7
myGitHubAkk <denusakkgit@gmail.com>
2021-09-14 23:27:26 +0300

    create colors

---
+++ lib/const/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const Color blackWhite = Color(0xffD7D7D7);
+const Color white = Color(0xffF9F9F9);
+
+const Color grey = Color(0xff474747);
+const Color blackGrey = Color(0xff000000);
+
+const Color purple = Color(0xffBD29CF);
+const Color blackPurple = Color(0xff48104F);
+
+const Color green = Color(0xff84CF3E);
+const Color blackGreen = Color(0xff158F24);
+
+const Color brown = Color(0xffA2533A);
+const Color blackBrown = Color(0xff783824);
+
+const Color riverDirtyColor = Color(0xff5AA36C);
+const Color riverMediumColor = Color(0xff5AA391);
+const Color riverCleanColor = Color(0xff6DB6D2);
